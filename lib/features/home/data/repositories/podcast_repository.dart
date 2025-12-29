2863f76c03203aabd63086bb329911895dfc4a4a
Adrian Castillo <acastillopedroza@gmail.com>
2025-12-29 18:29:43 -0500

    Add getPodcastById method to PodcastRepository

--- lib/features/home/data/repositories/podcast_repository.dart
+++ lib/features/home/data/repositories/podcast_repository.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:podcast_finder/features/home/data/data_sources/podcast_remote_data_source.dart';
+import 'package:podcast_finder/features/home/data/models/podcast_detail_model.dart';
 import '../models/podcast_model.dart';
 
 class PodcastRepository {
@@ -10,5 +11,10 @@ class PodcastRepository {
   Future<List<PodcastModel>> searchPodcasts(String query) async {
     await Future.delayed(Durations.long4);
     return _remoteDataSource.searchPodcasts(query);
+  }
+
+  Future<PodcastDetailModel> getPodcastById(String id) async {
+    await Future.delayed(Durations.long4);
+    return _remoteDataSource.getPodcastById(id);
   }
 }
