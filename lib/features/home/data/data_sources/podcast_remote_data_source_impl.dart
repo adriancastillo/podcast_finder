0e424fbb53b29fdfb979354be61191ce43a66682
Adrian Castillo <acastillopedroza@gmail.com>
2025-12-29 18:28:52 -0500

    Add getPodcastById API call with NetworkException handling to PodcastRemoteDataSource

--- lib/features/home/data/data_sources/podcast_remote_data_source_impl.dart
+++ lib/features/home/data/data_sources/podcast_remote_data_source_impl.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:podcast_finder/core/network/network_exceptions.dart';
 import 'package:podcast_finder/features/home/data/data_sources/podcast_remote_data_source.dart';
+import 'package:podcast_finder/features/home/data/models/podcast_detail_model.dart';
 import 'package:podcast_finder/features/home/data/models/podcast_model.dart';
 
 class PodcastRemoteDataSourceImpl implements PodcastRemoteDataSource {
@@ -14,6 +15,18 @@ class PodcastRemoteDataSourceImpl implements PodcastRemoteDataSource {
       final response = await _dio.get('/search', queryParameters: {'q': query});
       final results = response.data['results'] as List;
       return results.map((json) => PodcastModel.fromJson(json)).toList();
+    } on DioException catch (error) {
+      throw NetworkException.fromDioError(error);
+    } catch (_) {
+      throw const UnknownException();
+    }
+  }
+
+  @override
+  Future<PodcastDetailModel> getPodcastById(String podcastId) async {
+    try {
+      final response = await _dio.get('/podcasts/$podcastId');
+      return PodcastDetailModel.fromJson(response.data);
     } on DioException catch (error) {
       throw NetworkException.fromDioError(error);
     } catch (_) {
