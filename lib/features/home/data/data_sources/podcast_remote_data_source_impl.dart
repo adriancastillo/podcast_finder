173427e531c9ff2f23ff231ad84aee536fda1b7d
Adrian Castillo <acastillopedroza@gmail.com>
2025-12-29 18:26:40 -0500

    Map Dio errors to NetworkException in PodcastRemoteDataSource

--- lib/features/home/data/data_sources/podcast_remote_data_source_impl.dart
+++ lib/features/home/data/data_sources/podcast_remote_data_source_impl.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:podcast_finder/core/network/network_exceptions.dart';
 import 'package:podcast_finder/features/home/data/data_sources/podcast_remote_data_source.dart';
 import 'package:podcast_finder/features/home/data/models/podcast_model.dart';
 
@@ -9,8 +10,14 @@ class PodcastRemoteDataSourceImpl implements PodcastRemoteDataSource {
 
   @override
   Future<List<PodcastModel>> searchPodcasts(String query) async {
-    final response = await _dio.get('/search', queryParameters: {'q': query});
-    final results = response.data['results'] as List;
-    return results.map((json) => PodcastModel.fromJson(json)).toList();
+    try {
+      final response = await _dio.get('/search', queryParameters: {'q': query});
+      final results = response.data['results'] as List;
+      return results.map((json) => PodcastModel.fromJson(json)).toList();
+    } on DioException catch (error) {
+      throw NetworkException.fromDioError(error);
+    } catch (_) {
+      throw const UnknownException();
+    }
   }
 }
