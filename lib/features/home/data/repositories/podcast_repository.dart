5bd8cc117b0d328c60d5262e647d9881000b1cdd
Adrian Castillo <acastillopedroza@gmail.com>
2025-12-29 18:06:07 -0500

    Add PodcastRepository with searchPodcasts method

---
+++ lib/features/home/data/repositories/podcast_repository.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:podcast_finder/features/home/data/data_sources/podcast_remote_data_source.dart';
+import '../models/podcast_model.dart';
+
+class PodcastRepository {
+  const PodcastRepository(this._remoteDataSource);
+
+  final PodcastRemoteDataSource _remoteDataSource;
+
+  Future<List<PodcastModel>> searchPodcasts(String query) async {
+    await Future.delayed(Durations.long4);
+    return _remoteDataSource.searchPodcasts(query);
+  }
+}
