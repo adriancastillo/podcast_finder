b53da4aef234b54ea073e9cb7fc323b1d7070e1a
Adrian Castillo <acastillopedroza@gmail.com>
2025-12-29 18:36:22 -0500

    Enable podcast detail route in router configuration

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:go_router/go_router.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:podcast_finder/features/home/data/models/podcast_model.dart';
+import 'package:podcast_finder/features/home/presentation/screens/podcast_detail_screen.dart';
 import '../../features/home/presentation/screens/home_screen.dart';
 
 final appRouterProvider = Provider<GoRouter>((ref) {
@@ -10,15 +12,17 @@ final appRouterProvider = Provider<GoRouter>((ref) {
         path: '/',
         name: 'home',
         builder: (context, state) => const HomeScreen(),
+        routes: [
+          GoRoute(
+            path: '/podcast/:id',
+            name: 'detail',
+            builder: (context, state) {
+              final podcast = state.extra as PodcastModel;
+              return PodcastDetailScreen(podcast: podcast);
+            },
+          ),
+        ],
       ),
-      // GoRoute(
-      //   path: '/podcast/:id',
-      //   name: 'detail',
-      //   builder: (context, state) {
-      //     final id = state.pathParameters['id']!;
-      //     return PodcastDetailScreen(podcastId: id);
-      //   },
-      // ),
     ],
   );
 });
